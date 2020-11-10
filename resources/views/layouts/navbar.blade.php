59bc833810ce14a9bc3f23a7aaac3fd2be609cb8
Jahid-Hossain <jahidhossain073@gmail.com>
2020-11-11 00:45:43 +0600

    project1 commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,24 @@
+
+<header id="full_nav">
+
+<containar>
+    <row>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light t">
+  <a class="navbar-brand" href="#">BAF</a>
+  <a class="baf_icon"><i class="flaticon-bird"></i>
+    
+</a>
+  
+
+  <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item  ">
+          <a class="nav-link" href="#">Welcome To <span class="bangla">Bangla</span><span class="desh">desh</span> Airforce accommodation Management System<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </row>
+</containar>
+    
+</header>
